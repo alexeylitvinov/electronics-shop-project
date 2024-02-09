345827472471320616ccd088a2cf8a1cccb62ef8
alexeylitvinov <alextwin1976@gmail.com>
2024-02-09 15:04:05 +0100

    rework homework-5

--- src/keyboard.py
+++ src/keyboard.py
@@ -3,20 +3,20 @@ from src.item import Item
 
 class MixinChangeLang:
     def __init__(self):
-        self.list_lang = ['EN', 'RU']
+        self.__list_lang = ['EN', 'RU']
+        self.__language = self.__list_lang[0]
 
-
-class Keyboard(Item, MixinChangeLang):
-    def __init__(self, name, price, quantity):
-        super().__init__(name, price, quantity)
-        self.__language = self.list_lang[0]
+    def change_lang(self):
+        switch = self.__list_lang.pop(0)
+        self.__list_lang.append(switch)
+        self.__language = self.__list_lang[0]
+        return self.__language
 
     @property
     def language(self):
         return self.__language
 
-    def change_lang(self):
-        switch = self.list_lang.pop(0)
-        self.list_lang.append(switch)
-        self.__language = self.list_lang[0]
-        return self.__language
+
+class Keyboard(Item, MixinChangeLang):
+    def __init__(self, name, price, quantity):
+        super().__init__(name, price, quantity)
